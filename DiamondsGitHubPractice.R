a40afdb6fe13522e0abf63b4e67e90a0347eba19
Nylah <144974943+NylahS99@users.noreply.github.com>
2024-03-25 19:29:19 -0400

    Update DiamondsGitHubPractice.R

--- DiamondsGitHubPractice.R
+++ DiamondsGitHubPractice.R
@@ -9,3 +9,13 @@ glimpse()
 diamonds%>%
 select(x,y,z)%>%
 print(n = 30)
+
+
+
+library(data.table)
+
+diamond_table <- as.data.table(diamonds)
+
+result <- diamond_table[ cut == "Premium" & price > 7000 & price < 10000,.(carat, cut, price)][order(-carat)][1:20]
+
+print(result)
